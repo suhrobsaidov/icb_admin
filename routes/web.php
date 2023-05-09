6cdf9ae102d63aec0461d06fbddef72a1238bcff
lara <suhrob.aas@gmailcom>
2023-05-10 00:00:13 +0500

    all

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,87 @@ Route::get('/vacancy', function () {
 Route::get('/deposits', function () {
     return view('deposites');
 });
-
+Route::get('/term', function () {
+    return view('termdeposits');
+});
+Route::get('/show', function () {
+    return view('searchShow');
+});
+Route::get('/savings', function () {
+    return view('savingaccounts');
+});
+Route::get('/payroll', function () {
+    return view('payrollservices');
+});
+Route::get('/onlineservices', function () {
+    return view('onlineservices');
+});
+Route::get('/news', function () {
+    return view('news');
+});
+Route::get('/mortgate', function () {
+    return view('mortgateloan');
+});
+Route::get('/loans', function () {
+    return view('loans');
+});
+Route::get('/loanscalc', function () {
+    return view('loanscalculator');
+});
+Route::get('/donish', function () {
+    return view('loanproductdonish');
+});
+Route::get('/knowladge', function () {
+    return view('knowladgebase');
+});
+Route::get('/housingdeposit', function () {
+    return view('housingdeposit');
+});
+Route::get('/filiali', function () {
+    return view('filiali');
+});
+Route::get('/faq', function () {
+    return view('faq');
+});
+Route::get('/expresscredit', function () {
+    return view('expresscredit');
+});
+Route::get('/diyordeposit', function () {
+    return view('diyordeposit');
+});
+Route::get('/depositeasy', function () {
+    return view('depositeasy');
+});
+Route::get('/contacts', function () {
+    return view('contacts');
+});
+Route::get('/compliance', function () {
+    return view('compliance');
+});
+Route::get('/childdeposit', function () {
+    return view('childdeposit');
+});
+Route::get('/businessloan', function () {
+    return view('businessloan');
+});
+Route::get('/bankomati', function () {
+    return view('bankomati');
+});
+Route::get('/bankfinstatement', function () {
+    return view('bankfinstatement');
+});
+Route::get('/auto', function () {
+    return view('autoloan');
+});
+Route::get('/agrecultureloan', function () {
+    return view('agrecultureloan');
+});
+Route::get('/abouts', function () {
+    return view('abouts');
+});
+Route::get('/aboutplaceholders', function () {
+    return view('aboutplaceholders');
+});
 Route::get('/costumerloans', function () {
     return view('costumerloans');
 });
@@ -40,3 +120,7 @@ Route::get('/costumerloans', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::apiResource('sliders' , 'SLiderController');
+Route::apiResource('remitance' , 'RemitanceController');
+Route::apiResource('products' , 'ProductsController');
+Route::apiResource('services' , 'ServicesController');
