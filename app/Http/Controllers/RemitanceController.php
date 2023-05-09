acb7d8cfb1c71aec13fd045954cf580d83c9c0dc
lara <suhrob.aas@gmailcom>
2023-05-10 00:03:03 +0500

    all

---
+++ app/Http/Controllers/RemitanceController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Remitance;
+use Illuminate\Http\Request;
+
+class RemitanceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Remitance  $remitance
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Remitance $remitance)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Remitance  $remitance
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Remitance $remitance)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Remitance  $remitance
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Remitance $remitance)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Remitance  $remitance
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Remitance $remitance)
+    {
+        //
+    }
+}
---
+++ database/factories/RemitanceFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Remitance;
+use Faker\Generator as Faker;
+
+$factory->define(Remitance::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
