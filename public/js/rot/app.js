be04007893f3a718c331acea271453be60648f6b
timur-ur-nm <venom5st@mail.ru>
2023-07-05 16:09:37 +0500

    fix js

--- public/js/rot/app.js
+++ public/js/rot/app.js
@@ -3,7 +3,8 @@ const sliderNumbers = document.querySelectorAll('.slider-number');
 const interRateResult = document.querySelector('.inter-rate-result > div > span')
 const interRate = document.querySelector('.inter-rate > h2')
 
-
+// кнобки depositTermButtons
+const depositTermButtons = document.querySelectorAll('.deposit-term-buttons > button');
 
 
 sliders.forEach((slider, index) => {
@@ -11,3 +12,30 @@ sliders.forEach((slider, index) => {
     sliderNumbers[index].textContent = slider.value;
   });
 });
+
+
+
+// Обработчик события клика для кнопок depositTermButtons
+depositTermButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    depositTermButtons.forEach(btn => {
+      if (btn !== button) {
+        btn.classList.remove('black-button')
+      }
+    })
+    button.classList.add('black-button')
+  })
+})
+
+
+// const percent = document.querySelector('.inter-rate > h2')
+// const newPrecent = percent.textContent.substring(0,2).trim()
+// const resultSpan = document.querySelector('.inter-rate-result > div > span')
+
+// sliders.forEach((slider) => {
+//   slider.addEventListener('input', () => {
+//     let sliderN = Number(slider.value)
+//     let result = sliderN * newPrecent
+//     resultSpan.textContent = result
+//   });
+// });
