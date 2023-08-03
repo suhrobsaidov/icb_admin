d1305872ca5835abe5c252a4526844bacf045a26
lara <suhrob.aas@gmailcom>
2023-08-03 15:26:22 +0500

    trash clean

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,13 @@
-@extends('layouts.masters')
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
 
 @section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
 
-@endsection
+
